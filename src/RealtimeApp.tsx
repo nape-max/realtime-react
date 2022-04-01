64c52b656eecf3cb46a7986895d56ef9bd086555
Max Napedenin <nape.maxim@gmail.com>
2022-04-01 21:07:20 +0300

    Added root component for App.

---
+++ src/RealtimeApp.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function RealtimeApp() {
+  return <React.Fragment></React.Fragment>
+}
+
+export default RealtimeApp
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 // @ts-check
 
-import React from "react";
-import ReactDom from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-function Greeting(){
-  return <h4>Hello from New World!</h4>
-}
+import RealtimeApp from './RealtimeApp'
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+ReactDom.render(<RealtimeApp />, document.getElementById('root'))
