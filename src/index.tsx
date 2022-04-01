bf9421f9fe17b7721c7ddfb0997cd6d991cf22a5
Max Napedenin <nape.maxim@gmail.com>
2022-04-01 17:20:49 +0300

    Added tests outputs to browser.

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
+// @ts-check
+
 import React from "react";
+import ReactDom from 'react-dom';
+
+function Greeting(){
+  return <h4>Hello from New World!</h4>
+}
+
+ReactDom.render(<Greeting />, document.getElementById('root'))
